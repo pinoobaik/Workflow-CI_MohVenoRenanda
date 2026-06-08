1cf1d417464b1aecddf92d9a97e333c47b9f2d4a
pinoobaik <venorenand27@gmail.com>
2026-06-09 04:06:29 +0700

    tidak jadi menggunakan docker

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,6 +7,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+# Set tracking & artifact URI ke local relative (menghindari absolute path)
+mlflow.set_tracking_uri("./mlruns")
+mlflow.set_artifact_uri("./mlruns")
+
 mlflow.sklearn.autolog()
 
 df = pd.read_csv("telco_preprocessing.csv")
@@ -15,33 +19,17 @@ X = df.drop("Churn", axis=1)
 y = df["Churn"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 with mlflow.start_run():
-
-    model = RandomForestClassifier(
-        random_state=42
-    )
-
+    model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
-
     y_pred = model.predict(X_test)
-
     acc = accuracy_score(y_test, y_pred)
-
     print(f"Accuracy: {acc:.4f}")
-
     mlflow.log_metric("accuracy", acc)
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model"
-    )
-
+    # Simpan model sebagai file pkl untuk keperluan inference (opsional)
     joblib.dump(model, "model.pkl")
-
     mlflow.log_artifact("model.pkl")
