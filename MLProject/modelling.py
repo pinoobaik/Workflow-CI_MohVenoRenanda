69ddbf289c9384b43b78592c4eb1f69581457e42
pinoobaik <venorenand27@gmail.com>
2026-06-08 18:54:06 +0700

    Add MLflow CI workflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
+# aktifkan autolog
+mlflow.sklearn.autolog()
+
+# load dataset
+df = pd.read_csv("telco_preprocessing.csv")
+
+# split fitur dan target
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+with mlflow.start_run():
+
+    model = RandomForestClassifier(
+        random_state=42
+    )
+
+    model.fit(
+        X_train,
+        y_train
+    )
+
+    y_pred = model.predict(
+        X_test
+    )
+
+    acc = accuracy_score(
+        y_test,
+        y_pred
+    )
+
+    print(
+        f"Accuracy: {acc:.4f}"
+    )
