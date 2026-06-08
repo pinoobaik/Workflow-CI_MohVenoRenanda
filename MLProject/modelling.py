40be8d16d3ce4f490cbaf5c1aa52e7149d781f68
pinoobaik <venorenand27@gmail.com>
2026-06-08 20:27:45 +0700

    fix mlflow model logging

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,19 +1,16 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-
-# aktifkan autolog
 mlflow.sklearn.autolog()
 
-# load dataset
 df = pd.read_csv("telco_preprocessing.csv")
 
-# split fitur dan target
 X = df.drop("Churn", axis=1)
 y = df["Churn"]
 
@@ -30,20 +27,21 @@ with mlflow.start_run():
         random_state=42
     )
 
-    model.fit(
-        X_train,
-        y_train
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+
+    print(f"Accuracy: {acc:.4f}")
+
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
     )
 
-    y_pred = model.predict(
-        X_test
-    )
+    joblib.dump(model, "model.pkl")
 
-    acc = accuracy_score(
-        y_test,
-        y_pred
-    )
-
-    print(
-        f"Accuracy: {acc:.4f}"
-    )
+    mlflow.log_artifact("model.pkl")
